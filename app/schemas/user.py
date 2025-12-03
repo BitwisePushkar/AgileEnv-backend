5fc85fcc5d7de10d10d6c0ba46827e27d014a474
Pushkar Singhal <pusinghal010@gmail.com>
2025-12-03 16:48:03 +0530

    made basic crud apis

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel,Field,EmailStr,ConfigDict
+from typing import Optional
 
 class UserCreate(BaseModel):
     email: EmailStr
 
-#made this to hide data not return   
 class UserResponse(BaseModel):
     id:int
     email: EmailStr
-
     model_config = ConfigDict(from_attributes=True)
+
+class UserUpdate(BaseModel):
+    email: Optional[str] = None
