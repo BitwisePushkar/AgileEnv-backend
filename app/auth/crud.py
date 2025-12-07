0712e4f1d17c7fd8621db32354f6a59e81617d64
Agrim <agrimdubey37@gmail.com>
2025-12-07 15:43:30 +0530

    Added the crud operation of OTP

--- app/auth/crud.py
+++ app/auth/crud.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
-from app.auth.models import User,TokenBlackList
+from app.auth.models import User,TokenBlackList, OTP
 from app.auth.schemas import UserCreate
 from datetime import datetime, timezone, timedelta
+from random import randint
+
 
 def user_exist(db: Session, email: str):
     return db.query(User).filter(User.email == email,User.status == '1').first()
@@ -68,3 +70,51 @@ def add_token_blacklist(db:Session,token:str):
 def token_blacklisted(db:Session,token:str):
     result=db.query(TokenBlackList).filter(TokenBlackList.token==token).first()
     return result is not None
+
+
+def create_otp(db:Session, email:str, purpose:str):
+    otp_code  = randint(100000,999999)
+    created_at = datetime.now(timezone.utc)
+    expires_at = created_at + timedelta(minutes = 3)
+    db_otp = OTP(email=email,
+                 otp_code = str(otp_code),
+                 purpose = purpose,
+                 created_at = created_at,
+                 expires_at = expires_at
+                 )
+    db.add(db_otp)
+    db.commit()
+    db.refresh(db_otp)
+
+    return str(otp_code)
+    
+def verify_otp(db:Session, email:str, otp_code:str,purpose:str ):
+    db_otp = db.query(OTP).filter(OTP.email == email,
+                                  OTP.otp_code == otp_code,
+                                  OTP.purpose == purpose).first()
+    
+    if not db_otp:
+        return False
+    if db_otp.is_used:
+        return False 
+    if db_otp.expires_at <= datetime.now(timezone.utc):
+        return False
+    return True
+    
+
+def mark_otp_as_used(db:Session, email:str,purpose:str,otp_code:str):
+    db_otp = db.query(OTP).filter(OTP.email == email,
+                                  OTP.otp_code == otp_code,
+                                  OTP.purpose == purpose).first()
+    if db_otp:
+        db_otp.is_used=True
+        db.add(db_otp)
+        db.commit()
+        db.refresh(db_otp)
+        return True
+    return False
+
+
+
+
+    
