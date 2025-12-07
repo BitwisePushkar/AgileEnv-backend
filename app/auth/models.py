f85902906aecb83abd1bca2a141440e7d70741af
Agrim <agrimdubey37@gmail.com>
2025-12-07 15:17:29 +0530

    Added the model for OTP

--- app/auth/models.py
+++ app/auth/models.py
@@ -17,3 +17,14 @@ class TokenBlackList(Base):
     id=Column("Id",Integer,primary_key=True)
     token=Column("token",String(500),nullable=True)
     blacklisted_at=Column("blacklisted_at",DateTime,default=lambda:datetime.now(timezone.utc))
+
+class OTP(Base):
+    __tablename__ = "OTPs"
+    id = Column("Id",Integer, primary_key=True)
+    email = Column("Email",String(100),nullable = False, index= True)
+    otp_code = Column("Otp_code",String(6),nullable = False)
+    purpose = Column("Purpose",String(20),nullable = False)
+    created_at = Column("Created_at",DateTime,nullable= False, default = lambda: datetime.now(timezone.utc))
+    is_used = Column("Is_used",Boolean,default = False )
+    expires_at= Column("Expires_at",DateTime, nullable = False )
+
